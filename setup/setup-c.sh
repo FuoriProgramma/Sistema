f2213f2257a2fdb4f0e973e8a2c81e03f5a7c56a
Fuori Programma <fuoriprogramma.dev@gmail.com>
2021-07-26 18:16:11 +0200

    Optimized

--- setup/setup-c.sh
+++ setup/setup-c.sh
@@ -2,20 +2,17 @@
 
 sudo apt update
 
-echo "Installing Sublime text editor..."
+echo "Installing Sublime text editor repository..."
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
-sudo apt install -y sublime-text
 
-echo "Installing LLVM Toolchain..."
+echo "Installing LLVM Toolchain repository..."
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 echo "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-12 main" | sudo tee /etc/apt/sources.list.d/llvm-toolchain.list
-sudo apt update
-sudo apt install -y clang-12 clangd-12 lld-12 lldb-12
 
-echo "Installing CMake..."
-sudo apt install cmake ninja-build
+echo "Installing All packages..."
+sudo apt update
+sudo apt install -y cmake ninja-build clang-12 clangd-12 lld-12 lldb-12 sublime-text
 
 echo "export CC=clang-12" >> $HOME/.zshrc
 echo "export CXX=clang++-12" >> $HOME/.zshrc
