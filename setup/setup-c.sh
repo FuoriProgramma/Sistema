11a1fb0b48addb4bca753b13d5358923f494b8d8
Fuori Programma <fuoriprogramma.dev@gmail.com>
2021-07-27 14:54:41 +0200

    Usa il repository di default

--- setup/setup-c.sh
+++ setup/setup-c.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-echo "Installing LLVM Toolchain repository..."
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-echo "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-12 main" | sudo tee /etc/apt/sources.list.d/llvm-toolchain.list
-
 echo "Installing All packages..."
 sudo apt update
-sudo apt install -y cmake ninja-build clang-12 clangd-12 lld-12 lldb-12 sublime-text
+sudo apt install -y cmake ninja-build clang clangd lld lldb clang-format clang-tidy
 
 echo "export CC=clang-12" >> $HOME/.zshrc
 echo "export CXX=clang++-12" >> $HOME/.zshrc
