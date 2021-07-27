7c508990f8df2151f63807da51710a00a0a574c1
Fuori Programma <fuoriprogramma.dev@gmail.com>
2021-07-27 12:53:41 +0200

    Rimosso setup sublime-text (già presente di default)

--- setup/setup-c.sh
+++ setup/setup-c.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo apt update
-
-echo "Installing Sublime text editor repository..."
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
 echo "Installing LLVM Toolchain repository..."
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 echo "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-12 main" | sudo tee /etc/apt/sources.list.d/llvm-toolchain.list
